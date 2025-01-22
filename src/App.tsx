e309c1bf87bd09492e3ac39f80d0794d46961246
Yuri Kotenev <kotenev.yuri@yandex.ru>
2025-01-22 21:43:37 +0300

    add race condition sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import './App.css'
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const abortController = useRef<AbortController | null>(null);
+
+  const clearData = () => {
+    setData([]);
+    setError(null);
+    setLoading(false);
+    abortController?.current?.abort();
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+
+    abortController?.current?.abort();
+    abortController.current = new AbortController();
+
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts',
+        { signal: abortController.current.signal },
+      );
+
+      setData(response.data.map((post: any) => post.title));
+      setLoading(false);
+    } catch (err: any) {
+      if (err?.message !== 'canceled') {
+        setError('Failed to fetch data.');
+      }
+    }
+  };
 
   return (
-    <>
-        HELO WORLD
-    </>
-  )
-}
+    <div style={{ padding: '20px' }}>
+      <h1>HELLO WORLD</h1>
+      <button onClick={fetchData}>GET POSTS</button>
 
-export default App
+      <button onClick={clearData}>CLEAR DATA</button>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
