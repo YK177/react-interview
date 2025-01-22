b3756157e86bd41d547ad370e89ea76f2f3ec258
Yuri Kotenev <kotenev.yuri@yandex.ru>
2025-01-22 22:20:04 +0300

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import React, { useRef, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+
+// 'https://jsonplaceholder.typicode.com/posts',
 
 const App: React.FC = () => {
-  const [data, setData] = useState<string[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const clearData = () => {};
 
-  const abortController = useRef<AbortController | null>(null);
-
-  const clearData = () => {
-    setData([]);
-    setError(null);
-    setLoading(false);
-    abortController?.current?.abort();
-  };
-
-  const fetchData = async () => {
-    setLoading(true);
-    setError(null);
-
-    abortController?.current?.abort();
-    abortController.current = new AbortController();
-
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/posts',
-        { signal: abortController.current.signal },
-      );
-
-      setData(response.data.map((post: any) => post.title));
-      setLoading(false);
-    } catch (err: any) {
-      if (err?.message !== 'canceled') {
-        setError('Failed to fetch data.');
-        setLoading(false);
-      }
-    }
-  };
+  const fetchData = async () => {};
 
   return (
     <div style={{ padding: '20px' }}>
@@ -44,13 +13,6 @@ const App: React.FC = () => {
       <button onClick={fetchData}>GET POSTS</button>
 
       <button onClick={clearData}>CLEAR DATA</button>
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
     </div>
   );
 };
