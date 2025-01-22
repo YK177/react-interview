ec405305039d1ae4cf3b990a5d85a14d4a4d88bc
Yuri Kotenev <65911010+YK177@users.noreply.github.com>
2025-01-22 19:16:54 +0000

    add host

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ const App: React.FC = () => {
     } catch (err: any) {
       if (err?.message !== 'canceled') {
         setError('Failed to fetch data.');
+        setLoading(false);
       }
     }
   };
